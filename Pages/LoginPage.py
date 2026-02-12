8efff5f1f6792d5d45487d37f30fc847455825cf
ranvir.k <ranvir.sharma088@gmail.com>
2026-02-12 13:06:41 +0530

    Sasci

---
+++ Pages/LoginPage.py
@@ -0,0 +1,18 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+driver.implicitly_wait(10)
+driver.maximize_window()
+driver.get ("https://training.pfms.gov.in/SitePages/Users/LoginDetails/Login.aspx")
+
+
+#Test case = 1 Create Proposal
+
+driver.find_element(By.XPATH,"//input[@id='UserName']").send_keys("DLSFDU02")
+driver.find_element(By.XPATH,"//input[@id='Password']").send_keys("root@123")
+# here need to enter captcha manually
+time.sleep(5)
+driver.find_element(By.XPATH,"//input[@id='ctl00_cphBody_btnLoginButton']").click()
+time.sleep(2)
